12ffc923de4caddf8a989b938a97c89d22df3a34
bunandcheesenyam-cloud <bunandcheesenyam@gmail.com>
2026-04-02 10:41:58 -0500

    Add DISCORD_TOKEN fallback for Railway variable

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: (process.env.token || process.env.TOKEN || "").replace(/["']/g, ""), //- Bot's Token
+	token: (process.env.token || process.env.TOKEN || process.env.DISCORD_TOKEN || "").replace(/["']/g, ""), //- Bot's Token
 	clientId: (process.env.clientId || process.env.CLIENT_ID || "").replace(/["']/g, ""), //- ID of the bot
 	clientSecret: (process.env.clientSecret || process.env.CLIENT_SECRET || "").replace(/["']/g, ""), //- Client Secret of the bot
 	spotifyClientId: (process.env.SPOTIFY_CLIENT_ID || "").replace(/["']/g, ""), //- Spotify Client ID
