29de9111fa56f98b84c351db4d5f965d20b7c531
bunandcheesenyam-cloud <bunandcheesenyam@gmail.com>
2026-04-02 10:39:17 -0500

    Strip quotes from env variables automatically

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
 	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || process.env.TOKEN || "", //- Bot's Token
-	clientId: process.env.clientId || process.env.CLIENT_ID || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || process.env.CLIENT_SECRET || "", //- Client Secret of the bot
-	spotifyClientId: process.env.SPOTIFY_CLIENT_ID || "", //- Spotify Client ID
-	spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET || "", //- Spotify Client Secret
+	token: (process.env.token || process.env.TOKEN || "").replace(/["']/g, ""), //- Bot's Token
+	clientId: (process.env.clientId || process.env.CLIENT_ID || "").replace(/["']/g, ""), //- ID of the bot
+	clientSecret: (process.env.clientSecret || process.env.CLIENT_SECRET || "").replace(/["']/g, ""), //- Client Secret of the bot
+	spotifyClientId: (process.env.SPOTIFY_CLIENT_ID || "").replace(/["']/g, ""), //- Spotify Client ID
+	spotifyClientSecret: (process.env.SPOTIFY_CLIENT_SECRET || "").replace(/["']/g, ""), //- Spotify Client Secret
 	port: process.env.PORT || 4200, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
