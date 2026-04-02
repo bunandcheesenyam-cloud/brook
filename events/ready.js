d202faebf3c8673d979f643b395d55ec79245be4
bunandcheesenyam-cloud <bunandcheesenyam@gmail.com>
2026-04-02 11:42:55 -0500

    Move command auto-deployment entirely into ready.js event hook to guarantee commands are pushed regardless of Railway's custom Start Field overrides

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,27 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const LoadCommands = require("../util/loadCommands");
+
 /**
  *
  * @param {import("../lib/DiscordMusicBot")} client
  */
-module.exports = (client) => {
+module.exports = async (client) => {
 	client.manager.init(client.user.id);
 	client.user.setPresence(client.config.presence);
 	client.log("Successfully Logged in as " + client.user.tag);
+
+	client.log("Auto-deploying slash commands to Discord...");
+	try {
+		const rest = new REST({ version: "9" }).setToken(client.config.token);
+		const commands = await LoadCommands().then((cmds) => {
+			return [].concat(cmds.slash).concat(cmds.context);
+		});
+		await rest.put(Routes.applicationCommands(client.config.clientId), {
+			body: commands,
+		});
+		client.log("Successfully auto-deployed commands globally!");
+	} catch (err) {
+		client.error("Failed to auto-deploy commands: " + err);
+	}
 };
